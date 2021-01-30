3f90803ba563980f43f0b1b2a44ffdb70bdc003a
Michael La Dow <mpladow@gmail.com>
2021-01-30 14:57:11 +1100

    Init commit

---
+++ calendar-api/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calendar_api.Controllers
+{
+    [ApiController]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Authenticate(string username, string password)
+        {
+            // authenticate the user and return an auth token
+        }
+        public IActionResult Register(string username, string password)
+        {
+            // register a username and password
+        }
+    }
+}
